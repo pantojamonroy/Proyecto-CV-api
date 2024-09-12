47067c9a4ece921711794770d7cec3a430a50ff3
pantojamonroy <pantosaurio@gmail.com>
2024-09-11 19:15:24 -0500

    otro

--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -2,6 +2,6 @@ const misRutas = require("express").Router();
 
 const ContactsController = require("../controllers/contacts.controller");
 
-misRutas.post("/guardar", ContactsController.guardar());
+misRutas.post("/guardar", ContactsController.guardar);
 
 module.exports = misRutas;
